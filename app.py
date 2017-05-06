74c5c96ff8c71ea6046e87c33248f46fde7c088d
JesperGustafsson <jesper95gustafsson@gmail.com>
2017-05-06 17:38:15 +0200

    Loops all tests from RJTests and prints to Output.txt

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from Parsers import RJParser as rjParser
 
 if __name__ == '__main__':
     
-<<<<<<< HEAD:Mains/RJStarter.py
     finalString = ""
     output = open("Output.txt", "w")
     
@@ -20,7 +19,6 @@ if __name__ == '__main__':
     
     
     for test in tests[:]:
-        print "HMM"
         print test
         
     
@@ -43,25 +41,7 @@ if __name__ == '__main__':
         
         finalString += "\n##################\n\n\n\n\n"
         
-        output.write(str(returnString) + "\n##################\n\n\n\n\n")
-=======
-    moduleName = 'users'
-    hostname = "jesper-Aspire-E5-571"
+       # output.write(str(returnString) + "\n##################\n\n\n\n\n")
     
-    resultPath = "/home/stoff/TrueSec/result/"
-
-    
-    
-    file = open(resultPath + moduleName + "_info/" + hostname + ".log", "r")
-    method_to_call = getattr(RJParser, "read_" + moduleName + "_info")
-    dict = method_to_call(file)
-    
-    print "######################\n######################"
-    method_to_call = getattr(RJParser, "evaluate_" + moduleName + "_info")
-    returnString = method_to_call(dict)
-    
-    print returnString
->>>>>>> f83909a01ec5794381742cac7d3ab001ae8a6a17:app.py
-    
-    
+    output.write(finalString)
     pass

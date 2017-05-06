6c0b80970ec9793433310ad38c3fcebdac6925ab
JesperGustafsson <jesper95gustafsson@gmail.com>
2017-05-06 15:26:56 +0200

    Translated java to Python

--- Mains/RJStarter.py
+++ Mains/RJStarter.py
@@ -6,16 +6,16 @@ Created on May 3, 2017
 
 import Parsers
 from Parsers.RJParser import *
-from Parsers import RJParser
+from Parsers import RJParser as rjParser
 
 if __name__ == '__main__':
     
-    moduleName = 'firewall'
+    moduleName = 'users'
     hostname = "jesper-Aspire-E5-571"
     
     resultPath = "/home/jesper/Documents/remote_job_linux_osx-master-d98598cf1ea7ba905e77c62efb3b52a4bcacf366/result/"
     
-    rjParser = RJParser
+
     
     
     file = open(resultPath + moduleName + "_info/" + hostname + ".log", "r")

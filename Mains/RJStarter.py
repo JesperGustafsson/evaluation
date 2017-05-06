0b8c2a3b041bb7a14004f3e63a696f8f2a81fd0f
JesperGustafsson <jesper95gustafsson@gmail.com>
2017-05-06 17:21:13 +0200

    Fixade lite

--- Mains/RJStarter.py
+++ Mains/RJStarter.py
@@ -10,23 +10,38 @@ from Parsers import RJParser as rjParser
 
 if __name__ == '__main__':
     
-    moduleName = 'users'
-    hostname = "jesper-Aspire-E5-571"
+    finalString = ""
+    output = open("Output.txt", "w")
     
-    resultPath = "/home/jesper/Documents/remote_job_linux_osx-master-d98598cf1ea7ba905e77c62efb3b52a4bcacf366/result/"
+    tests = [i[:-1] for i in open("RJTests")]
+    print tests
     
-
     
+    for test in tests[:]:
+        print "HMM"
+        print test
+        
     
-    file = open(resultPath + moduleName + "_info/" + hostname + ".log", "r")
-    method_to_call = getattr(rjParser, "read_" + moduleName + "_info")
-    dict = method_to_call(file)
-    
-    print "######################\n######################"
-    method_to_call = getattr(rjParser, "evaluate_" + moduleName + "_info")
-    returnString = method_to_call(dict)
-    
-    print returnString
+        moduleName = test
+        hostname = "jesper-Aspire-E5-571"
+        
+        resultPath = "/home/jesper/Documents/remote_job_linux_osx-master-d98598cf1ea7ba905e77c62efb3b52a4bcacf366/result/"
+        
+        finalString += "### " + moduleName + " ###\n\n"
+        
+        file = open(resultPath + moduleName + "_info/" + hostname + ".log", "r")
+        method_to_call = getattr(rjParser, "read_" + moduleName + "_info")
+        dict = method_to_call(file)
+        
+        print "######################\n######################"
+        method_to_call = getattr(rjParser, "evaluate_" + moduleName + "_info")
+        returnString = method_to_call(dict)
+        
+        finalString += str(returnString)
+        
+        finalString += "\n##################\n\n\n\n\n"
+        
+        output.write(str(returnString) + "\n##################\n\n\n\n\n")
     
     
     pass
